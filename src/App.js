8c5aa4e4d2cd49b921424e5010032dd45a0f50de
Kylon Chiang <kylonc@andrew.cmu.edu>
2020-10-07 09:58:09 -0400

    Finished lab

--- src/App.js
+++ src/App.js
@@ -24,32 +24,60 @@ const default_todoList = [
   }
 ];
 
+const localStorageName = "todo";
+
 //TODO 3: Make it so that the user's todos load back when they refresh
 //Hint: use localStorage.setItem/getItem and JSON.stringify/parse
 //  if you mess up setting the local storage you can .clear() it
 
 class App extends Component {
+  storedList = localStorage.getItem(localStorageName);
   state = {
-    todoList: default_todoList,
+    todoList: this.storedList ? JSON.parse(this.storedList) : default_todoList,
     newTodoContent: ""
   };
+
   toggleDone = (evt, i) => {
     console.log("done", i);
     let todoList = this.state.todoList;
     todoList[i].finished = !todoList[i].finished;
     this.setState({ todoList: todoList });
+
+    window.localStorage.setItem(localStorageName, JSON.stringify(todoList));
   };
+
   addItem = () => {
     //TODO 1: Make the add button work (hint: this.setState())
     console.log("AddItem", this.state.newTodoContent);
+    const { todoList, newTodoContent } = this.state;
+    todoList.push({
+      content: newTodoContent,
+      finished: false
+    })
+    this.setState({
+      todoList
+    });
+
+    window.localStorage.setItem(localStorageName, JSON.stringify(todoList));
   };
   deleteItem = (event, i) => {
     //TODO 2: Make the delete button work (hint: event.stopPropagation())
-    console.log("delete", i);
+    event.stopPropagation();
+
+    let { todoList } = this.state;
+    console.log("Current todo:", todoList);
+    todoList.splice(i, 1);
+    // todoList.slice(0, i).push(todoList.slice(i + 1, todoList.length));
+    console.log("Modified todo:", todoList);
+    // console.log("delete", i);
+    this.setState({ todoList });
+
+    window.localStorage.setItem(localStorageName, JSON.stringify(todoList));
   };
 
   render() {
     console.log("rerender");
+    console.log(this.state.todoList);
     let list_content = [];
     for (let i = 0; i < this.state.todoList.length; i++) {
       let todo = this.state.todoList[i];
@@ -62,7 +90,7 @@ class App extends Component {
           {/* Comment: Below the "Done!" icon is conditionally rendered */}
           {todo.finished && <div className="DoneIcon">Done!</div>}
           <div className="Filler"></div>
-          <div className="DeleteIcon" onClick={(e) => console.log("delete")}>
+          <div className="DeleteIcon" onClick={(event) => this.deleteItem(event, i)}>
             {"x"}
           </div>
         </li>
